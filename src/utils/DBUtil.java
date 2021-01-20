1394e9d68e58d9d8afe928b0b098457f72d56fd3
hiroki.kaneko <kanekohiroki@kanekohirokinoMacBook-Pro.local>
2021-01-20 17:37:35 +0900

    Add getAlltasks

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -5,14 +5,14 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class DBUtil {
-    private static final String PERSISTENSE_UNIT_NAME = "tasks";
+    private static final String PERSISTENSE_UNIT_NAME = "tasklist";
     private static EntityManagerFactory emf;
 
     public static EntityManager createEntityManager() {
         return getEntityManagerFactory().createEntityManager();
     }
 
-    private static EntityManagerFctory getEntityManagerFactory() {
+    private static EntityManagerFactory getEntityManagerFactory() {
         if(emf == null) {
             emf = Persistence.createEntityManagerFactory(PERSISTENSE_UNIT_NAME);
         }
